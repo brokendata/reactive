23574ab50dbca494f6ab0b8f2d4f38eccd9462b6
Ryan Wheeler <ryanmwheeler@gmail.com>
2015-04-15 08:25:18 -0500

    week 0 tests passing

--- week0/example/src/test/scala/example/ListsSuite.scala
+++ week0/example/src/test/scala/example/ListsSuite.scala
@@ -36,7 +36,7 @@ class ListsSuite extends FunSuite {
    * which tests that its argument evaluates to `true`. So one of the simplest
    * successful tests is the following:
    */
-  test("one plus one is two")(assert(1 + 1 == 2))
+  test("one plus one is two")(assert(1 + 1 === 2))
 
   /**
    * In Scala, it is allowed to pass an argument to a method using the block
@@ -45,7 +45,7 @@ class ListsSuite extends FunSuite {
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 2 == 3) // This assertion fails! Go ahead and fix it.
   }
 
   /**
@@ -69,7 +69,7 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 === 2) // Fix me, please!
   }
 
   /**
