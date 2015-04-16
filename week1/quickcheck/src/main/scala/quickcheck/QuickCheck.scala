0936ed81f59c95750ecf5ee2bb7f90af4779b9cb
Ryan Wheeler <ryanmwheeler@gmail.com>
2015-04-16 08:18:11 -0500

    5 tests passing on week1

--- week1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ week1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,12 +14,18 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = oneOf(const(empty), genHeap)
-
+  lazy val genHeap: Gen[H] = for {
+    k <- arbitrary[Int]
+    m <- oneOf(const(empty), genHeap)
+  } yield insert(k,m)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-
+  //Given in assignment
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h))==m
+  }
 
 
   property("hint1") = forAll { (n: Int, m: Int) =>
@@ -31,6 +37,24 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   property("hint2") = forAll{ (n: Int) =>
     val h = insert(n,empty)
     deleteMin(h) == empty
+  }
+
+  property("hint3") = forAll {(ls: List[Int]) =>
+    def list2heap(ls: List[Int]): H = ls match {
+      case Nil => empty
+      case x :: xs => insert(x,list2heap(xs))
+    }
+
+    def heap2list(h: H): List[Int] = {
+      def loop(h: H, acc: List[Int]): List[Int]=
+        if (isEmpty(h)) acc
+        else loop(deleteMin(h),findMin(h) :: acc)
+      loop(h,Nil)
+    }
+
+    val heap = list2heap(ls)
+    heap2list(heap) == ls.sorted
+
   }
 
 
