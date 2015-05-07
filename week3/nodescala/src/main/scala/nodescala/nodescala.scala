bb74d55b787a8b69252050fcca9cfaa67b267587
Ryan Wheeler <ryanmwheeler@gmail.com>
2015-05-07 14:06:43 -0500

    moar

--- week3/nodescala/src/main/scala/nodescala/nodescala.scala
+++ week3/nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,13 @@ trait NodeScala {
    *  @param token        the cancellation token
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    response.foreach{iter =>
+      if(!token.isCancelled) exchange.write(iter)
+      else exchange.close
+    }
+    exchange.close
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -41,8 +47,12 @@ trait NodeScala {
    *  @param handler        a function mapping a request to a response
    *  @return               a subscription that can stop the server and all its asynchronous operations *entirely*
    */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    val listenerSub = listener.start
 
+
+  }
 }
 
 
